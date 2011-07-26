b3174fa0c620b3de87ca65b4485c1aed23e4d0b4
Emmanuel Bernard <emmanuel@hibernate.org>
2011-07-26 23:55:01 +0200

    Add disqus comments

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,6 @@
 require 'mytagger'
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
   extension Awestruct::Extensions::Posts.new( '/news', :posts )
   extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5 )
   extension Awestruct::Extensions::MyTagger.new( :posts, 
@@ -10,6 +9,7 @@ Awestruct::Extensions::Pipeline.new do
                                                :per_page=>5 )
   extension Awestruct::Extensions::Atomizer.new( :posts, '/news/news.atom' )
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Disqus.new
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::GoogleAnalytics
 end
